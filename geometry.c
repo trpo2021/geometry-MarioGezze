59079b50801b5f63ffa7848d9007420a1da47e1c
MarioGezze <mariogezze71147@gmail.com>
2021-02-25 22:55:09 +0600

    trying to add translation function

--- geometry.c
+++ geometry.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-bool finding_bracket(char* UKAZAT)
+bool finding_bracket(char* UKAZAT) //поиск скобки
 {
     int flag = 0;
     while (*UKAZAT != 10) {
@@ -20,7 +20,7 @@ bool finding_bracket(char* UKAZAT)
     return true;
 }
 
-bool finding_bracket2(char* UKAZAT)
+bool finding_bracket2(char* UKAZAT) //поиск второй скобки
 {
     int flag = 0;
     while (*UKAZAT != 10) {
@@ -36,7 +36,7 @@ bool finding_bracket2(char* UKAZAT)
     return true;
 }
 
-bool finding_comma(char* UKAZAT)
+bool finding_comma(char* UKAZAT) //поиск запятой
 {
     int flag = 0;
     while (*UKAZAT != 10) {
@@ -53,7 +53,8 @@ bool finding_comma(char* UKAZAT)
 }
 
 bool correct(char* Circle, char* UKAZAT, int N)
-{
+{ //проверка правильности вводимых данных
+
     int g = 6, correct = 0, check = 0;
     if (correct == 0) {
         if (strncmp(Circle, UKAZAT, g) != 0) {
@@ -73,11 +74,29 @@ bool correct(char* Circle, char* UKAZAT, int N)
     if (check != 3)
         return false;
 }
+char* translation_x(char* UKAZAT, double* x)
+{ //переводим х из текста в число
+
+    char* endptr;
+    while (isdigit(*UKAZAT) == 0) {
+        if ((*UKAZAT == ' ') || (*UKAZAT == '(')) {
+            UKAZAT++;
+        } else {
+            printf("expected '<double>'\n");
+            return 0;
+        }
+    }
+    if (isdigit(*UKAZAT) != 0) {
+        *x = strtod(UKAZAT, &endptr);
+        UKAZAT = endptr;
+    }
+    return UKAZAT;
+}
 
 int main()
 {
     int N = 50;
-    double x, y, radius;
+    double *x, y, radius;
     char str[N];
     char* UKAZAT = str;
     char* UKAZAT2 = str;
@@ -85,15 +104,19 @@ int main()
     char Circle[] = {"circle"};
     int circlen = strlen(Circle);
 
-    fgets(str, N, stdin);
-    if (isalpha(*UKAZAT) != 0) {
+    fgets(str, N, stdin); //ввод данных
+    if (isalpha(*UKAZAT) != 0) { //передвигаем указатель на первую скобку
         while (isalpha(*UKAZAT2) != 0)
             UKAZAT2++;
     }
     if (correct(Circle, str, N) == true) {
+        //если проверка прошла успешно, передвигаем первый указатель
+        //на первую скобку
         UKAZAT = UKAZAT2;
         printf("correct");
     }
+    translation_x(UKAZAT, x);
+    printf("%f\n", x);
     system("pause");
     return 0;
 }
