871709df29a08463bfc82cd3f8110420b03936a2
MarioGezze <mariogezze71147@gmail.com>
2021-02-26 21:35:02 +0600

    it work, but need some collor fix

--- geometry.c
+++ geometry.c
@@ -1,4 +1,5 @@
 #include <ctype.h>
+#include <math.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -52,40 +53,35 @@ bool finding_comma(char* UKAZAT) //поиск запятой
 bool correct(char* Circle, char* UKAZAT, int N)
 { //проверка правильности вводимых данных
 
-    int g = 6, correct = 0, check = 0;
-    if (correct == 0) {
-        if (strncmp(Circle, UKAZAT, g) != 0) {
-            printf("error: Check the spelling of the word  -circle- ");
-        } else {
-            check++;
-        }
+    int g = 6, check = 0;
+    if (strncmp(Circle, UKAZAT, g) == 0) {
+        check++;
         if (finding_bracket(UKAZAT) == true)
             check++;
         if (finding_comma(UKAZAT) == true)
             check++;
         if (finding_bracket2(UKAZAT) == true)
             check++;
+    } else {
+        printf("error: Check the spelling of the command");
     }
     if (check == 4)
         return true;
     if (check != 4)
         return false;
+    return true;
 }
-void translation_universal(char* UKAZAT, double* x, double* x1)
+void translation_universal(char* UKAZAT, double* x1)
 { //переводим х из текста в число
     char* point;
     double c;
     int i = 0;
     while (isdigit(*UKAZAT) == 0) {
         UKAZAT++;
-        fputs(UKAZAT, stdout);
         if (isdigit(*UKAZAT) != 0) {
-            printf("111\n");
             c = strtod(UKAZAT, &point);
             x1[i] = c;
-            printf("%f", x1[i]);
             i++;
-            printf("222\n");
             UKAZAT = point;
             if (i == 3)
                 break;
@@ -96,17 +92,24 @@ void translation_universal(char* UKAZAT, double* x, double* x1)
         }
     }
 }
-
+void calculation(double* A)
+{
+    float pi = 3.1415;
+    float S;
+    float P;
+    P = 2 * pi * A[2];
+    S = pi * pow(A[2], 2);
+    printf("Perimetr = %f\n", P);
+    printf("Area = %f\n", S);
+}
 int main()
 {
     int N = 50;
-    double *x, *x1, *y, *radius, A[4];
+    double A[4];
     char str[N];
     char* UKAZAT = str;
     char* UKAZAT2 = str;
-    int i = 0;
     char Circle[] = {"circle"};
-    int circlen = strlen(Circle);
 
     fgets(str, N, stdin); //ввод данных
     if (isalpha(*UKAZAT) != 0) { //передвигаем указатель на первую скобку
@@ -117,10 +120,10 @@ int main()
         //если проверка прошла успешно, передвигаем первый указатель
         //на первую скобку
         UKAZAT = UKAZAT2;
-        printf("correct");
+        printf("correct\n");
     }
-    translation_universal(UKAZAT, x, A);
-    printf("%f\n", A[i + 2]);
+    translation_universal(UKAZAT, A);
+    calculation(A);
     system("pause");
     return 0;
 }
